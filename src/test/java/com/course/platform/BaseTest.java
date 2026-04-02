a75cb1eb2ceb8e9fb356dc4bc6b7a07102358491
George Chatzidakis <g.chatzidakis@outlook.com.gr>
2026-04-02 19:14:06 +0300

    add test containers

--- src/test/java/com/course/platform/BaseTest.java
+++ src/test/java/com/course/platform/BaseTest.java
@@ -1,19 +1,25 @@
 package com.course.platform;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 @SpringBootTest()
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 @Transactional
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public abstract class BaseTest {
 
     @Autowired
@@ -29,5 +35,28 @@ public abstract class BaseTest {
         public ObjectMapper objectMapper() {
             return new ObjectMapper();
         }
+    }
+
+    // PostgreSQL container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15")
+            .withDatabaseName("testdb")
+            .withUsername("test")
+            .withPassword("test");
+
+    @BeforeAll
+    static void startContainer() {
+        postgres.start();
+    }
+
+    // Inject container properties into Spring
+    @DynamicPropertySource
+    static void configureDatabase(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("spring.datasource.driver-class-name", postgres::getDriverClassName);
+
+        // Hibernate config
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "create-drop");
     }
 }
